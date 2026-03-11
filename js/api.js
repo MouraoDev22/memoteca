7a61f8ddf9b614e7c0db1fbd39b7412be06255de
Mourão <p3dr0sk0sk7160@gmail.com>
2026-03-11 19:52:24 -0300

    requisição POST feita

--- js/api.js
+++ js/api.js
@@ -9,6 +9,24 @@ const api = {
             alert(message.error);
             return null;
         };
+    },
+
+    async salvarPensamento(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(pensamento)
+            });
+            const data = await response.json();
+            return data;
+        } catch(error) {
+            console.error(error);
+            alert(message.error);
+            return null;
+        };
     }
 }
 
