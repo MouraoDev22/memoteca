9163e809d0520a789fbfbf610b062ca968845f7a
Mourão <p3dr0sk0sk7160@gmail.com>
2026-03-12 17:52:06 -0300

    feitos métodos de buscarPensamentoPorId e editarPensamento

--- js/api.js
+++ js/api.js
@@ -27,7 +27,37 @@ const api = {
             alert(message.error);
             return null;
         };
-    }
+    },
+
+    async buscarPensametoPorId(id) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`);
+            const data = await response.json();
+            return data;
+        } catch(error) {
+            console.error(error);
+            alert(message.error);
+            return null;
+        };
+    },
+
+    async editarPensamento(pensamento) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(pensamento)
+            });
+            const data = await response.json();
+            return data;
+        } catch(error) {
+            console.error(error);
+            alert(message.error);
+            return null;
+        };
+    },
 }
 
 export default api;
